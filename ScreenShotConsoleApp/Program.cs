1935480fa962c1cd7decd83d140a6093d2c185d1
sankarananth <006ananth@gmail.com>
2020-03-27 22:14:45 +0530

    New Changes

--- ScreenShotConsoleApp/Program.cs
+++ ScreenShotConsoleApp/Program.cs
@@ -22,15 +22,11 @@ namespace DriveQuickstart
         // at ~/.credentials/drive-dotnet-quickstart.json
         static string[] Scopes = { DriveService.Scope.Drive};
         static string ApplicationName = "Drive API .NET Quickstart";
-
-        static void Main(string[] args)
+        UserCredential credential;
+        public  UserCredential GetCredential()
         {
-            PrintScreen ps = new PrintScreen();
-            ps.CaptureScreenToFile("screen.jpg", ImageFormat.Jpeg);
-            UserCredential credential;
-
             using (var stream =
-                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+               new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
             {
                 // The file token.json stores the user's access and refresh tokens, and is created
                 // automatically when the authorization flow completes for the first time.
@@ -43,53 +39,148 @@ namespace DriveQuickstart
                     new FileDataStore(credPath, true)).Result;
                 Console.WriteLine("Credential file saved to: " + credPath);
             }
-            var fileMetadata = new File()
-            {
-                Name = "screenshot.jpg",
-            };
-            FilesResource.CreateMediaUpload request;
-            string path = "C:\\ss\\screen.jpeg";
-           
+            return credential;
 
-            // Create Drive API service.
-            var service = new DriveService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = ApplicationName,
-            });
-
-            // Define parameters of request.
-            FilesResource.ListRequest listRequest = service.Files.List();
-            listRequest.PageSize = 10;
-            listRequest.Fields = "nextPageToken, files(id, name)";
-            using (var stream = new System.IO.FileStream(path,
-                                   System.IO.FileMode.Open))
-            {
-                long len=stream.Length;
-                request = service.Files.Create(
-                    fileMetadata, stream, "image/jpeg");
-                request.Fields = "id";
-                request.Upload();
-            }
-            var response = request.ResponseBody;
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
             
-
-            // List files.
-            IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute()
-                .Files;
-            Console.WriteLine("Files:");
-            if (files != null && files.Count > 0)
+            string folderCreation = CreateFolder("Screenshot");
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            PrintScreen ps = new PrintScreen();
+            ps.CaptureScreenToFile(path + "\\screen.jpg", ImageFormat.Jpeg);
+            if (folderCreation!= "Sorry but the file Screenshot already exists!")
             {
-                foreach (var file1 in files)
+                
+                
+                var fileMetadata = new File()
                 {
-                    Console.WriteLine("{0} ({1})", file1.Name, file1.Id);
+                    Name = "screenshot.jpg",
+                    Parents = new List<string> { folderCreation }
+                };
+                FilesResource.CreateMediaUpload request;
+                // Create Drive API service.
+                var service = new DriveService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = p.GetCredential(),
+                    ApplicationName = ApplicationName,
+                });
+
+                // Define parameters of request.
+                FilesResource.ListRequest listRequest = service.Files.List();
+                listRequest.PageSize = 10;
+                listRequest.Fields = "nextPageToken, files(id, name)";
+                using (var stream = new System.IO.FileStream(path + "\\screen.jpg",
+                                       System.IO.FileMode.Open))
+                {
+                    //fileMetadata.Parents.Add()
+                    request = service.Files.Create(
+                        fileMetadata, stream, "image/jpeg");
+                    request.Fields = "id";
+                    request.Upload();
                 }
+
+
+                // List files.
+                IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute()
+                    .Files;
+                Console.WriteLine("Files:");
+                if (files != null && files.Count > 0)
+                {
+                    foreach (var file1 in files)
+                    {
+                        Console.WriteLine("{0} ({1})", file1.Name, file1.Id);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No files found.");
+                }
+                Console.Read();
+
             }
             else
             {
-                Console.WriteLine("No files found.");
+                var service = new DriveService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = p.GetCredential(),
+                    ApplicationName = ApplicationName,
+                });
+                var listRequest = service.Files.List();
+                listRequest.PageSize = 100;
+                listRequest.Q = "trashed = false and name contains 'Screenshot' and 'root' in parents";
+                listRequest.Fields = "files";
+                var files = listRequest.Execute().Files;
+                string id = "";
+                foreach (var file in files)
+                {
+                    if ("Screenshot" == file.Name)
+                        id=file.Id;
+
+                }
+                var fileMetadata = new File()
+                {
+                    Name = "screenshot.jpg",
+                    Parents = new List<string> { id }
+                };
+                
+                Console.WriteLine("Folder Already Exists");
+                FilesResource.CreateMediaUpload request;
+                using (var stream = new System.IO.FileStream(path + "\\screen.jpg",
+                                       System.IO.FileMode.Open))
+                {
+                    //fileMetadata.Parents.Add()
+                    request = service.Files.Create(
+                        fileMetadata, stream, "image/jpeg");
+                    request.Fields = "id";
+                    request.Upload();
+                }
             }
-            Console.Read();
+            
+           
+            
+        }
+        public static string CreateFolder(string folderName)
+        {
+            Program p = new Program();
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = p.GetCredential(),
+                ApplicationName = ApplicationName,
+            });
+            bool exists = Exists(folderName);
+            if (exists)
+                return $"Sorry but the file {folderName} already exists!";
+
+            var file = new Google.Apis.Drive.v3.Data.File();
+            file.Name = folderName;
+            file.MimeType = "application/vnd.google-apps.folder";
+            var request = service.Files.Create(file);
+            request.Fields = "id";
+            return request.Execute().Id;
+        }
+        private static bool Exists(string name)
+        {
+            Program p = new Program();
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = p.GetCredential(),
+                ApplicationName = ApplicationName,
+            });
+            var listRequest = service.Files.List();
+            listRequest.PageSize = 100;
+            listRequest.Q = $"trashed = false and name contains '{name}' and 'root' in parents";
+            listRequest.Fields = "files(name)";
+            var files = listRequest.Execute().Files;
+            string id = "";
+            foreach (var file in files)
+            {
+                if (name == file.Name)
+                    return true;
+                
+            }
+            return false;
         }
     }
 }
